23121305b232c46973a7cde2d7470f7f22999af1
Jeffrey Lee <darkthread@gmail.com>
2021-05-03 20:42:47 +0800

    embedding Newtonsoft.Json.dll

--- JsonToolkitCom/JsonXmlConverter.cs
+++ JsonToolkitCom/JsonXmlConverter.cs
@@ -1,10 +1,9 @@
 ﻿using Newtonsoft.Json;
 using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Globalization;
+using System.IO;
+using System.Reflection;
 using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
 using System.Xml.Linq;
 
 namespace JsonToolkitCom
@@ -26,14 +25,44 @@ namespace JsonToolkitCom
     [ComDefaultInterface(typeof(IJsonXmlConverter))]
     public class JsonXmlConverter : IJsonXmlConverter
     {
-        public string JsonToXml(string xml)
+        public string XmlToJson(string xml)
         {
             return JsonConvert.SerializeXNode(XDocument.Parse(xml).Root);
         }
 
-        public string XmlToJson(string json)
+        public string JsonToXml(string json)
         {
             return JsonConvert.DeserializeXNode(json).ToString();
         }
+
+        static JsonXmlConverter()
+        {
+            AppDomain.CurrentDomain.AssemblyResolve += OnResolveAssembly;
+        }
+
+        private static Assembly OnResolveAssembly(object sender, ResolveEventArgs args)
+        {
+            Assembly executingAssembly = Assembly.GetExecutingAssembly();
+            AssemblyName assemblyName = new AssemblyName(args.Name);
+
+            string path = assemblyName.Name + ".dll";
+            if (assemblyName.CultureInfo.Equals(CultureInfo.InvariantCulture) == false)
+            {
+                path = String.Format(@"{0}\{1}", assemblyName.CultureInfo, path);
+            }
+
+            using (Stream stream = executingAssembly.GetManifestResourceStream(path))
+            {
+                if (stream == null)
+                    return null;
+
+                byte[] assemblyRawBytes = new byte[stream.Length];
+                stream.Read(assemblyRawBytes, 0, assemblyRawBytes.Length);
+                return Assembly.Load(assemblyRawBytes);
+            }
+        }
+
     }
+
+
 }
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# CCWDemo
+# COM Callable Wrapper (CCW) Example
+
+A CCW project example with 3rd party assembly referenced.
+
+[turorial](https://blog.darkthread.net/blogs/ccw-example) (in Chinese)
